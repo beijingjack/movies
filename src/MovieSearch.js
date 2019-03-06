c367bea340aed972738ec826e53136c80c6c8f8a
Ljy <Jingyu@LjydeAir.volo.net>
2019-03-06 16:58:09 -0600

    build the router

---
+++ src/MovieSearch.js
@@ -0,0 +1,99 @@
+import React, { Component } from 'react';
+import axios from 'axios'
+import Movies from './Movies'
+import './App.css';
+
+class MovieSearch extends Component {
+	constructor(){
+		super();
+		this.state = {
+			value: '',
+			movies: [],
+		};
+		this.inputChangeHandler = this.inputChangeHandler.bind(this);
+		this.popularMovies = this.popularMovies.bind(this);
+		this.latestClickHandler = this.latestClickHandler.bind(this);
+		this.oldestClickHandler = this.oldestClickHandler.bind(this);
+		this.baseUrl = 'https://api.themoviedb.org/3/';
+	}
+
+	inputChangeHandler(event) {
+		this.setState({ value: event.target.value });
+		this.searchMovie()
+	}
+
+	searchMovie(){
+		if(this.state.value === '') {
+			return
+		}
+		let url = `${this.baseUrl}search/movie?api_key=57597e10d2a4be7b31ce5f3098929194&language=en-US&query=${this.state.value}&include_adult=false`;
+		axios.get(url).then((response) => {
+			this.setState({
+				movies: response.data.results
+			});
+		}).catch((error) => {
+			console.log(error);
+		});
+	}
+
+	popularMovies() {
+		let url = `${this.baseUrl}movie/popular?api_key=57597e10d2a4be7b31ce5f3098929194&language=en-US`;
+		axios.get(url).then((response) => {
+			this.setState({
+				movies: response.data.results
+			});
+		}).catch((error) => {
+			console.log(error);
+		});
+	}
+
+	sortDateClickHandler(i) {
+		let movies = this.state.movies;
+		movies.sort((a,b) => {
+			if (a.release_date<b.release_date) {return i}
+			if (a.release_date>b.release_date) {return -i}
+			return 0
+		});
+		this.setState({
+			movies: movies
+		});
+	}
+
+	latestClickHandler(){
+		this.sortDateClickHandler(1);
+	}
+	oldestClickHandler(){
+		this.sortDateClickHandler(-1);
+	}
+
+	render() {
+		//let popularMovies = this.popularMovies();
+		return (
+			<div>
+				<h2>Search Movies...</h2>
+				<input
+					value={this.state.value}
+					placeholder='Search Movie Here...'
+					onChange={this.inputChangeHandler}
+				/>
+				<button onClick={this.latestClickHandler}>
+					Sort From Newest
+				</button>
+
+				<button onClick={this.oldestClickHandler}>
+					Sort From Oldest
+				</button>
+
+
+				<Movies
+					movies={this.state.movies}
+					value={this.state.value}
+					popularMovies={this.popularMovies}
+				/>
+			</div>
+
+		);
+	}
+}
+
+export default MovieSearch;
