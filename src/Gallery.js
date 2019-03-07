2aa16f55d6d9429560e661c1dace76ab5ff926a7
Ljy <Jingyu@LjydeAir.volo.net>
2019-03-07 11:20:11 -0600

    implemented the movie search

--- src/Gallery.js
+++ src/Gallery.js
@@ -14,15 +14,15 @@ class Gallery extends Component{
 
 	async popularMovies() {
 		let url = `${this.baseUrl}movie/popular?api_key=57597e10d2a4be7b31ce5f3098929194&language=en-US&`;
+		let moviePages = [];
 		let movies = [];
-		movies.push(await axios.get(`${url}page=1`).then((response) => response.data.results));
-		movies.push(await axios.get(`${url}page=2`).then((response) => response.data.results));
-		movies.push(await axios.get(`${url}page=3`).then((response) => response.data.results));
-		movies.push(await axios.get(`${url}page=4`).then((response) => response.data.results));
-		movies.push(await axios.get(`${url}page=5`).then((response) => response.data.results));
-		this.setState({
-			movies: movies
-		})
+		moviePages.push(await axios.get(`${url}page=1`).then((response) => response.data.results));
+		moviePages.push(await axios.get(`${url}page=2`).then((response) => response.data.results));
+		moviePages.push(await axios.get(`${url}page=3`).then((response) => response.data.results));
+		moviePages.push(await axios.get(`${url}page=4`).then((response) => response.data.results));
+		moviePages.push(await axios.get(`${url}page=5`).then((response) => response.data.results));
+		for (const page of moviePages) {page.map(movie => movies.push(movie))}
+		this.setState({movies: movies});
 	}
 
 	render() {
--- src/GalleryMovies.js
+++ src/GalleryMovies.js
@@ -3,16 +3,13 @@ import Movie from "./Movie";
 
 class GalleryMovies extends Component {
 	render() {
-		//this.props.popularMovies();
 		let moviesView;
 		const noMovie = this.props.movies.length === 0;
 		if (noMovie) {
-			console.log('here');
 			this.props.popularMovies();
 		}
 		else{
 			moviesView = this.props.movies.map((movie, idx) => {
-				console.log(movie);
 				return (
 					<li key={idx}>
 						<Movie movie={movie}/>
