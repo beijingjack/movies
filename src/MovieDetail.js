9ca13115f3bc3243aa4c41e5520fe05fac931ec0
Ljy <Jingyu@LjydeAir.volo.net>
2019-03-08 01:20:42 -0600

    works on detail

--- src/MovieDetail.js
+++ src/MovieDetail.js
@@ -6,23 +6,23 @@ class MovieDetail extends Component{
 	constructor() {
 		super();
 		this.state = {
-			id: parseInt(this.props.match.params.id),
 			movie:[]
 		};
 		this.baseUrl = 'https://api.themoviedb.org/3/movie/';
 	}
 
 	getMovie() {
-		let url = `${this.baseUrl}${this.state.id}?api_key=57597e10d2a4be7b31ce5f3098929194&language=en-US`;
+		let id = this.props.match.params.id;
+		let url = `${this.baseUrl}${id}?api_key=57597e10d2a4be7b31ce5f3098929194&language=en-US`;
 		axios.get(url).then((response) => {
 			this.setState({
 				movie: response.data
 			});
 		})
 	}
+
 	render() {
 		return(
-			//console.log(this.props.match.params.id) // it is a string
 			<MovieDetailView
 				movie={this.state.movie}
 				getMovie={() => this.getMovie()}
--- src/MovieDetailView.js
+++ src/MovieDetailView.js
@@ -1,6 +1,26 @@
 import React, { Component } from 'react';
 
 class MovieDetailView extends Component {
+	render() {
+		let imageUrl;
+		const noMovie = this.props.movie.length === 0;
+		if (noMovie) {
+			this.props.getMovie();
+		}else {
+			imageUrl = `https://image.tmdb.org/t/p/w300_and_h450_bestv2${this.props.movie.poster_path}`;
+		}
+		return(
+			//use flex box
+			<div>
+				<img
+					src={imageUrl}
+					alt={`${this.props.movie.title}`}
+				/>
+				<h2>Rating : {this.props.movie.vote_average}</h2>
+				<h3>{this.props.movie.overview}</h3>
+			</div>
+		)
+	}
 
 }
 
